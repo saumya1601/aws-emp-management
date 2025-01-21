2b2f63047dac0d4118d031699792f23b8495cabe
Saumya Modi <saumya20244@gmail.com>
2025-01-21 17:41:01 +0530

    Refactor Next.js configuration to include output setting directly in the main config object

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,11 +1,22 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: ['res.cloudinary.com'],
+//   },
+// };
+// nextConfig.exports = {
+//   output: 'export'
+// }
+
+// export default nextConfig;
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ['res.cloudinary.com'],
   },
+  output: 'export', // This should be part of the main configuration object
 };
-nextConfig.exports = {
-  output: 'export'
-}
 
 export default nextConfig;
